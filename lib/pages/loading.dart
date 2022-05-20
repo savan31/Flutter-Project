f4e23b1ee1cc99b37cfbfaae402b0e0059089bf8
Savan Parvadiya <Sparvadiya7502@conestogac.on.ca>
2022-05-20 12:14:30 -0400

    How to get data from http link or API in flutter

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,6 +10,21 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async{
+      Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+      print(response.body);
+      Map data = jsonDecode(response.body);
+      print(data['title']);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
