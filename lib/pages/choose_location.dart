e20c1f9442e3a694bd273b6669bd9de322d1b481
Savan Parvadiya <Sparvadiya7502@conestogac.on.ca>
2022-05-20 11:00:56 -0400

    How to write Asynchronus code and using of aWait

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,10 +8,44 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async{
+    String userName =await Future.delayed(Duration(seconds: 3),(){
+      return 'John';
+    });
+
+    String bioData = await Future.delayed(Duration(seconds: 2),(){
+      return 'John';
+    });
+
+    print('$userName : $bioData');
+  }
+  int addition = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('buid funcation called');
     return Scaffold(
-      body: Text('Choose Location Page'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location'),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: RaisedButton(
+        onPressed: (){
+          setState(() {
+            addition++;
+          });
+        },
+        child:Text('Addition is $addition'),
+      ),
     );
   }
 }
