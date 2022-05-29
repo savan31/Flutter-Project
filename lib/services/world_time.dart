4032a3a436aeaee489546ca11a35280b6f82fa5f
Savan Parvadiya <Sparvadiya7502@conestogac.on.ca>
2022-05-28 22:47:57 -0400

    Use of try catch and Future

---
+++ lib/services/world_time.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
+
+
+class WorldTime {
+
+  late String location; //for UI
+  late String time ; //Live time of location which is inserted
+  late String flag ; //url to  an asset flag icon
+  late String url ; //location url from API endpoint
+
+  WorldTime({required this.location,required this.flag,required this.url});
+
+  Future<void> getTime() async{
+    //Future will force dart to run that and we can use await
+    try{
+      //make the request
+      Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
+
+      //store properties from data
+      String dateTime = data['utc_datetime'];
+      String offSet = data['utc_offset'].substring(1,3);
+      // print(dateTime + "  offset :"+ offSet);
+
+      //create date time object
+      DateTime liveTime = DateTime.parse(dateTime);
+      liveTime = liveTime.subtract(Duration(hours: int.parse(offSet)));
+      // print(liveTime);
+
+      time = liveTime.toString();
+    }
+    catch(e){
+      print('Error for getting data : $e');
+      time = 'Something went wrong in getting time';
+    }
+
+  }
+
+}
